06cfd23c2da92123f4dbb1f4ac00fcb4fc0e8c7f
EgorMezhin <egorswege@yandex.com>
2021-05-31 00:06:17 +0300

    Лекция 13 ДЗ+

--- L13Homework/Lecture13Homework/Lecture13Homework/ViewController.swift
+++ L13Homework/Lecture13Homework/Lecture13Homework/ViewController.swift
@@ -8,25 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     private lazy var treeView: UIImageView = {
-        let treeImage = UIImageView(frame: CGRect(
-                                        x: self.view.frame.midX,
-                                        y: self.view.frame.midY,
-                                        width: 0,
-                                        height: 0))
+        let treeImage = UIImageView(
+            frame: CGRect(
+                x: self.view.frame.midX,
+                y: self.view.frame.midY,
+                width: 0,
+                height: 0))
         treeImage.image = UIImage(named: "tree")
         treeImage.contentMode = .scaleAspectFit
         return treeImage
     }()
     
     private lazy var bananaView: UIImageView = {
-        var bananaImage = UIImageView(frame: CGRect(
-                                        x: self.view.frame.midX,
-                                        y: self.view.frame.midY,
-                                        width: 0,
-                                        height: 0))
+        var bananaImage = UIImageView(
+            frame: CGRect(
+                x: self.view.frame.midX + 60,
+                y: self.view.frame.midY - 20,
+                width: 0,
+                height: 0))
         bananaImage.image = UIImage(named: "banana")
+        bananaImage.contentMode = .scaleAspectFit
+        return bananaImage
+    }()
+    
+    private lazy var appleView: UIImageView = {
+        var bananaImage = UIImageView(
+            frame: CGRect(
+                x: self.view.frame.midX - 20,
+                y: self.view.frame.midY - 100,
+                width: 0,
+                height: 0))
+        bananaImage.image = UIImage(named: "apple")
         bananaImage.contentMode = .scaleAspectFit
         return bananaImage
     }()
@@ -37,12 +51,11 @@ class ViewController: UIViewController {
         view.backgroundColor = .cyan
         view.addSubview(treeView)
         view.addSubview(bananaView)
+        view.addSubview(appleView)
         animate()
     }
     
     func animate() {
-        //  treeView.layer.removeAllAnimations()
-        
         UIView.animate(
             withDuration: 5,
             delay: 0,
@@ -52,12 +65,18 @@ class ViewController: UIViewController {
             self.treeView.bounds.size.height = self.view.frame.height
             self.treeView.bounds.size.width = self.view.frame.width
         }
-        
-        UIView.animate(withDuration: 3, delay: 0, options: .curveEaseOut) {
-            [weak self] in
-            guard let self = self else { return }
-            self.bananaView.bounds.size.height = 50
-            self.bananaView.bounds.size.width = 50
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            UIView.animate(
+                withDuration: 3,
+                delay: 0,
+                options: .curveEaseOut) {
+                [weak self] in
+                guard let self = self else { return }
+                self.bananaView.bounds.size.height = 50
+                self.bananaView.bounds.size.width = 50
+                self.appleView.bounds.size.height = 50
+                self.appleView.bounds.size.width = 50
+            }
         }
     }
 }
